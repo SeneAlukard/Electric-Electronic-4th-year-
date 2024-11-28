18bcebf9dbdaf584ba7b6291d270066fda58e899
Senealukard <koc.ezenarslan@protonmail.com>
2024-11-29 01:28:02 +0300

    Update: Assigment 2 finished.

---
+++ EE449/Assigment2/matrixfy.m
@@ -0,0 +1,15 @@
+function x = matrixfy(max_num, target_matrix)
+
+  x = zeros(max_num, size(target_matrix, 2));
+
+  for i = 1:size(target_matrix, 2)
+
+    x(target_matrix(i)+1,i) = 1;
+
+  end
+
+end
+
+
+
+
